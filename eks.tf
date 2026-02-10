02128f846d17d0f68178b01bb340a6e71146c68d
Sundarapandian <sundarakce@gmail.com>
2026-02-10 14:15:22 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -36,7 +36,7 @@ module "eks" {
       desired_size = 1
 
       instance_types = ["t3.small"]
-      capacity_type  = "SPOT"
+      capacity_type  = "ON_DEMAND"
 
       tags = {
         ExtraTag = "helloworld"
